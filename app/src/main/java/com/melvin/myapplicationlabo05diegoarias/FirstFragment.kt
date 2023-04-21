9cae7a680d34d5933b167c70690ec851bcd2a9c9
Gabriel Rodriguez <00005118@uca.edu.sv>
2023-04-21 11:03:44 -0600

    first commit

---
+++ app/src/main/java/com/melvin/myapplicationlabo05diegoarias/FirstFragment.kt
@@ -0,0 +1,39 @@
+package com.melvin.myapplicationlabo05diegoarias
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class FirstFragment : Fragment() {
+
+    private lateinit var btnActionSend: FloatingActionButton
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnActionSend = view.findViewById(R.id.buttton_first_fragment)
+
+        btnActionSend.setOnClickListener {
+            it.findNavController().navigate(R.id.action_firstFragment_to_secondFrgament)
+        }
+    }
+
+    private fun bind(){
+        btnActionSend = view?.findViewById(R.id.buttton_first_fragment) as Button
+    }
+
+}
